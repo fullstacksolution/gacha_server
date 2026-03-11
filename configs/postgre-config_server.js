1743a358c5d2ba6c6975630b4a6a57340997c78b
FullstackDev <122088759+fullstacksolution@users.noreply.github.com>
2026-03-11 17:31:06 +0900

    update/server info

--- configs/postgre-config_server.js
+++ configs/postgre-config_server.js
@@ -1,8 +1,11 @@
 module.exports = {
-  "username": "gacha_admin",
-  "password": "bqo7UWQnvfFrJmwOOK39Fa0k6KIPPQ9X",
-  "database": "gacha_db_yi3q",
-  "host": "dpg-d4gmjo2li9vc73dnala0-a",
+  "username": "gacha_admin_user",
+  // "password": "bqo7UWQnvfFrJmwOOK39Fa0k6KIPPQ9X",
+  // "database": "gacha_db_yi3q",
+  // "host": "dpg-d4gmjo2li9vc73dnala0-a",
+  "password": "52TwQ2jJw7EydHbAQzeH4hx4C08oMLdn",
+  "database": "gacha_admin",
+  "host": "dpg-d6oia7ma2pns738emql0-a.singapore-postgres.render.com",
   "dialect": "postgres",
   "dialectOptions": {
     ssl: {
